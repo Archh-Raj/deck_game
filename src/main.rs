6ae1cf554d4a5a95051b40026018e0cd722a32fb
Archh-Raj <as_rajkumar@outlook.com>
2025-09-13 19:02:56 +0530

    created deck struct and added some cards in the deck

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    println!("Hello, world!");
-    println!("hello")
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
 }
+
+
+fn main() {
+
+    let suits = ["Heart","Diamond","Spade"];
+    let values = ["Ace", "One", "Two", "Three"];
+
+    let mut cards = vec![];
+
+    for suit in suits{
+        for value in values{
+            let card = format!("{} of {}", suit, value);
+
+            cards.push(card);
+        }
+    }   
+
+    let deck:Deck = Deck{ cards };
+    println!("Here is your deck: {:#?}", deck);
+}
+
+
